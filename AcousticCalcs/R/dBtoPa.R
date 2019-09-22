804d92bfea3e038f2b9826321d2e42dd39b3310a
Jeff Zeyl <jeffzeyl@gmail.com>
2019-09-22 18:57:55 +0200

    Add files via upload

---
+++ AcousticCalcs/R/dBtoPa.R
@@ -0,0 +1,23 @@
+#description - converts dB to Pa
+#input - dB value and reference micropascals
+#output - pascals value
+
+#' Convert dB to Pascals
+#'
+#' This function allows you to convert decibels to Pascals. Defaults to a reference pressure of 20 microPascals.
+#' @param dB decibel value
+#' @param ref reference pressure in micropasals
+#' @export
+#' @examples
+#' dBtoPa(94,20)
+#' 1.002374
+
+#arguments: dB value, reference units
+dBtoPa<- function (dB,ref = 20) {
+  uPa<-20*10^(dB/ref)
+  Pa<-uPa/1000000
+  #print( ifelse( missing(dB), 'Pa is not specified', paste('a =',a) ) )
+  return(Pa)
+}
+#dBtoPa(93.97,20)
+#dBtoPa()
---
+++ AcousticCalcs/README.md
@@ -0,0 +1,15 @@
+# AcousticCalcs
+Set of R functions for common calculations in acoustics and audiometry
+
+```
+Installation:
+library(devtools)
+devtools::install_github("jzeyl/AcousticCalcs", force = TRUE)
+```
+
+Functions:
+>dBtoPa()
+
+>PatodB()
+
+>pressuredrop()
