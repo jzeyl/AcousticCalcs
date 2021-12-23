cdffb379bfce55e1dede5e3a905bc1e9bde40604
= <jeffzeyl@gmail.com>
2021-12-23 11:27:36 -0500

    fix human_audiogram.R

--- R/human audiogram.R
+++ R/human audiogram.R
@@ -7,8 +7,8 @@
 human_audiogram<-function(){
 Hz<-c(4,8,16,32,64,125,250,500,1000,2000,3000,4000,8000,16000,17690.086)
 Threshold<-c(105.35573,93.794464,82.49012,59.624504,36.501976,16.976284,10.296443,9.782609,-3.320158,-9.486166,-9.8712885,-10.256411,7.6923075,25.641026,71.02564)
-audiogram<-data.frame(cbind(Hz,Threshold))
-audiogram<-data.frame(approx(audiogram$Hz,audiogram$threshold,n = 5000))
+#audiogram<-data.frame(cbind(Hz,Threshold))
+audiogram<-as.data.frame(approx(Hz,Threshold,n = 5000))
 colnames(audiogram)<-c("Hz","Threshold")
 return(audiogram)
 }
